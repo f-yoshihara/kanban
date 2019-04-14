3be4f0302c1344190bbd7482f1d4f8ec6e6a1657
Fumiro Yoshihara <fumiro1989@gmail.com>
2019-04-14 19:12:17 +0900

    php artisan make:model Task --migration

--- app/Task.php
+++ app/Task.php
@@ -2,32 +2,9 @@
 
 namespace App;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Task
- * @package App
- * @property int    user_id
- * @property string title
- * @property string note
- * @property int    order
- * @property Carbon done_at
- * @property Carbon created_at
- * @property Carbon updated_at
- */
 class Task extends Model
 {
-    protected $fillable = [
-        'user_id',
-        'title',
-        'note',
-        'order'
-    ];
-
-    protected $dates = [
-        'done_at',
-        'created_at',
-        'updated_at'
-    ];
+    //
 }
