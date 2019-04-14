aea1e6995fda02f7be0e1a512badc437b8cb05a1
Fumiro Yoshihara <fumiro1989@gmail.com>
2019-04-14 19:58:20 +0900

    Taskモデル・migrationファイル編集

--- app/Task.php
+++ app/Task.php
@@ -2,9 +2,32 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Task
+ * @package App
+ * @property int    user_id
+ * @property int    parent_id
+ * @property string title
+ * @property string note
+ * @property Carbon started_at
+ * @property Carbon done_at
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'note'
+    ];
+
+    protected $dates = [
+        'started_at',
+        'done_at',
+        'created_at',
+        'updated_at'
+    ];
 }
--- database/migrations/2019_04_14_191146_create_tasks_table.php
+++ database/migrations/2019_04_14_191146_create_tasks_table.php
@@ -15,6 +15,14 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->foreign('parent_id')->references('id')->on('tasks');
+            $table->string('title');
+            $table->text('note')->nullable();
+            $table->dateTime('started_at')->nullable();
+            $table->dateTime('done_at')->nullable();
             $table->timestamps();
         });
     }
